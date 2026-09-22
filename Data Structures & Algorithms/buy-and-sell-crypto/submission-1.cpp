fe44fb528fdd0839aed3ccbe05c1c3127940344e
Aidan D <aidandecina11@gmail.com>
2026-09-22 17:01:42 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0;
+        int r = 1;
+        int mProfit = 0;
+        
+        while (r < prices.size()) {
+            if (prices[r] > prices[l]) {
+                mProfit = max(mProfit, prices[r] - prices[l]);
+            }
+            else {
+                l = r;
+            }
+            r++;
+            
+        }
+
+        
+        return mProfit;
+    }
+};
